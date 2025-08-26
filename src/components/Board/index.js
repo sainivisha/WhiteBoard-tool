a717ea0bf2d4066130bf00f8707fe0ae74dd2c3c
sainivisha <sainivishaa@gmail.com>
2025-08-26 16:11:09 +0530

    Brush Tool created in the whiteboard-tool

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useLayoutEffect, useRef } from "react";
 import rough from "roughjs";
 import boardContext from "../../store/board-context";
-import { TOOL_ACTION_TYPES } from "../../constants";
+import { TOOL_ACTION_TYPES, TOOL_ITEMS } from "../../constants";
 import toolboxContext from "../../store/toolbox-context";
 
 function Board() {
@@ -27,9 +27,25 @@ function Board() {
     const context = canvas.getContext("2d");
     context.save();
     const roughCanvas = rough.canvas(canvas);
+
     elements.forEach((element) => {
-      roughCanvas.draw(element.roughEle);
+      switch (element.type) {
+        case TOOL_ITEMS.LINE:
+        case TOOL_ITEMS.RECTANGLE:
+        case TOOL_ITEMS.CIRCLE:
+        case TOOL_ITEMS.ARROW:
+          roughCanvas.draw(element.roughEle);
+          break;
+        case TOOL_ITEMS.BRUSH:
+          context.fillStyle = element.stroke;
+          context.fill(element.path);
+          context.restore();
+          break;
+        default:
+          throw new Error("Type not recognized");
+      }
     });
+
     return () => {
       context.clearRect(0, 0, canvas.width, canvas.height);
     };
